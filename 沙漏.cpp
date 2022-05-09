bda7ad89aa935e08785942314873aeec4afc5c87
earthbound1984 <91579482+earthbound1984@users.noreply.github.com>
2022-05-09 21:21:15 +0800

    Add files via upload

---
+++ 沙漏.cpp
@@ -0,0 +1,36 @@
+﻿
+
+#include <stdio.h>
+
+int main()
+{
+	int a,i,n,m;
+	printf("请输入一个奇数\n");
+	scanf_s("%d", &a);
+	for (i = 1; i <= (a + 1)/2; ++i)
+	{
+		for (m = 1; m <= i - 1; ++m)
+		{
+			printf(" ");
+		}
+		for (n = 1; n <= a - 2 * (i - 1); ++n)
+		{
+			printf("*");
+		}
+		printf("\n");
+	}//上半部分
+	for (i = 1; i <= (a + 1) / 2 - 1; ++i)
+	{
+		for (m = 1; m <=(a-1)/2-i; ++m)
+		{
+			printf(" ");
+		}
+		for (n = 1; n <= 2 * i + 1; ++n)
+		{
+			printf("*");
+		}
+		printf("\n");
+	}//下半部分
+}
+
+
